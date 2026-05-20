0b786c17be925065a529358efaaf7fb2609c82e0
rasindujayasinghe757-dotcom <rasindujayasinghe757@gmail.com>
2026-05-20 13:38:14 +0530

    Update session.js

--- session.js
+++ session.js
@@ -37,6 +37,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "your session",
+SESSION_ID: process.env.SESSION_ID || "XPRO-MD~1779264294414_d03hi0z7l",
 PORT: process.env.PORT || "8000"
 };
